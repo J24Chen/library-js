714335a580f5f615e999f3a8213795ebd82be374
J24Chen <Jchen2408@gmail.com>
2024-10-29 11:47:50 -0400

    Finish basic functionality TODO: Actually style it lol

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const test1 = new Book("Siddartha","Herman Hesse",295,"read");
+const test1 = new Book("Siddartha","Herman Hesse",295,"Read");
 const test2 = new Book("Bone","Jeff Smith",150,"Not Read");
-const test3 = new Book("The Sympathizer","Viet Thanh Nguyen",350,"read");
+const test3 = new Book("The Sympathizer","Viet Thanh Nguyen",350,"Read");
 let myLibrary = [test1,test2,test3];
 
-const testButton = document.querySelector(".testButton");
+
 const cardContainer = document.querySelector(".card-container");
+
+const newBookButton = document.querySelector(".new-book");
+const dialog = document.querySelector("dialog");
+const closeButton = document.querySelector(".close-button")
+const submitButton = document.querySelector(".submit-button");
+
+const form = document.querySelector('form')
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pageInput = document.querySelector("#pageNum");
+const readInput = document.querySelector("#readInput");
 
 function Book(name,author,pageNum,readStatus) {
     this.name = name;
@@ -13,15 +24,38 @@ function Book(name,author,pageNum,readStatus) {
     this.readStatus = readStatus;
 }
 
+newBookButton.addEventListener("click",() => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener("click", () =>{
+    dialog.close();
+})
+
+submitButton.addEventListener("click", (e) =>{
+    if(!form.checkValidity()) {
+        form.reportValidity();
+    } else {
+        e.preventDefault(); 
+        addToLibrary(titleInput.value,authorInput.value,pageInput.value,readInput.checked);
+        dialog.close();
+    }
+    
+
+    
+
+});
 
 function addToLibrary(name,author,pageNum,readStatus) {
     let newBook = new Book(name,author,pageNum,readStatus)
-
     myLibrary.push(newBook);
+    createBookDisplay();
 }
 
 
-/*Params: bookObj | book Objet to be removed from myLibrary array
+/*
+function: removeBook
+Params: bookObj | book Objet to be removed from myLibrary array
           cardDiv | card Div that holds entire book information.
 
 Deletes bookObj from array, cardDiv from DOM 
@@ -33,13 +67,25 @@ function removeBook(bookObj,cardDiv){
     console.log(myLibrary);
 }
 
-function toggleStatus(){
+function toggleStatus(div){
+    //This is hardcoded, learn how to fix this to be better
+    let readSpan = div.childNodes[3];
+    if(readSpan.textContent == "Status: Read"){
+        readSpan.textContent = "Status: Not Read";
+    } else {
+        readSpan.textContent = "Status: Read";
+    }
 
+}
+
+function refreshBookDisplay() {
+    cardContainer.innerHTML ="";
 }
 
 /*Iterates through the myLibrary array, creating a div card for each book.
 */
 function createBookDisplay(){
+    refreshBookDisplay();
     myLibrary.forEach( (i) => {
         let div = document.createElement("div");
         div.classList.add("card");
@@ -47,10 +93,11 @@ function createBookDisplay(){
         let nameSpan = document.createElement("span");
         let authorSpan = document.createElement("span");
         let pageSpan = document.createElement("span");
-
+        let readSpan = document.createElement("span");
+        
         let delButton = document.createElement("button");
         let statusToggle = document.createElement("button");
-
+        
         nameSpan.classList.add("title");
         console.log(i); 
         console.log(i.author);
@@ -62,11 +109,13 @@ function createBookDisplay(){
         delButton.addEventListener("click", (e) => {
             removeBook(i,e.target.parentNode);
         });
-        statusToggle.addEventListener("click",() => {
-            toggleStatus(i,e.target)
+        statusToggle.addEventListener("click",(e) => {
+            toggleStatus(e.target.parentNode)
         });
+        statusToggle.textContent = "Change Read Status";
         delButton.textContent = "Remove";
-        readSpan.textContent = "toggleRead";
+        
+        
         
 
         div.appendChild(nameSpan);
@@ -74,18 +123,18 @@ function createBookDisplay(){
         div.appendChild(pageSpan);
         div.appendChild(readSpan);
         div.appendChild(delButton);
-        div.appendChild(readSpan);
+        div.appendChild(statusToggle);
         cardContainer.appendChild(div);
         
     });
 
 }
 
-testButton.addEventListener("click", createBookDisplay());
-/* TODO:
-Create a function that displays card array
-    With button that deletes it 
-    And button that toggles the read/unread button.
+createBookDisplay();
 
-Go from there
+
+/*
+TODO:
+add requirement stars on each form,
+style the rest of css.
 */
